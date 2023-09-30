8c1375dbf3eb9dbeb1e1d5d5138f5c6549d37700
Giridhar Nair <Giridhar.r.nair@gmail.com>
2023-09-30 15:17:30 -0500

    Hopefully final

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# AudioTranscribe
-# AudioTranscribe
+# Audio Transcriber Backend
+
+This is the README for the Audio Transcriber Backend, which is a Flask-based application that transcribes audio files and sends the transcription via email. The backend also performs additional processing on the transcription, including summarization, key point extraction, action item identification, and sentiment analysis. Below, you will find information on how to set up and use this backend.
+
+## Prerequisites
+
+Before you can run the Audio Transcriber Backend, make sure you have the following prerequisites installed:
+
+- Python 3.x
+- Flask
+- Flask-CORS
+- SendGrid Python Library
+- OpenAI Python Library
+- PyDub
+- Python `dotenv` library
+
+You also need to set up accounts and obtain API keys for the following services:
+
+- [OpenAI](https://beta.openai.com/signup/)
+- [SendGrid](https://sendgrid.com/)
+
+Once you have these prerequisites, replace the placeholders in your `.env` file with your actual API keys.
+
+## Installation
+
+1. Clone this repository to your local machine.
+
+   ```bash
+   git clone https://github.com/your_username/your_repo.git
+   ```
+
+2. Navigate to the project directory.
+
+   ```bash
+   cd your_repo
+   ```
+
+3. Create a virtual environment (recommended).
+
+   ```bash
+   python -m venv venv
+   ```
+
+4. Activate the virtual environment.
+
+    - On Windows:
+
+      ```bash
+      venv\Scripts\activate
+      ```
+
+    - On macOS and Linux:
+
+      ```bash
+      source venv/bin/activate
+      ```
+
+5. Install the required dependencies.
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+1. Start the Flask server by running the following command:
+
+   ```bash
+   python app.py
+   ```
+
+   The server will start at `http://localhost:5000`.
+
+2. Make a POST request to the `/transcribe` endpoint with the following data:
+
+    - `email`: The recipient's email address.
+    - `audio_file`: The audio file to be transcribed (supported formats: mp3, mp4, mpeg, wav, flac, ogg, mpga, m4a, webm).
+
+   Example using `curl`:
+
+   ```bash
+   curl -X POST -F "email=recipient@example.com" -F "audio_file=@/path/to/audio/file.wav" http://localhost:5000/transcribe
+   ```
+
+   This will submit a transcription request. The backend will transcribe the audio, summarize it, extract key points, identify action items, and perform sentiment analysis on the transcription. The results will be emailed to the provided email address.
+
+## Supported Audio Formats
+
+The backend supports the following audio formats for transcription:
+
+- mp3
+- mp4
+- mpeg
+- wav
+- flac
+- ogg
+- mpga
+- m4a
+- webm
+
+## Customization
+
+- You can customize the AI models used for summarization, key point extraction, action item identification, and sentiment analysis by modifying the respective functions in the `app.py` file.
+- You can also customize the email sender's address and the subject line for the email in the `send_email` function.
+
+## Error Handling
+
+- The backend provides error handling for missing required fields, invalid file formats, and internal server errors. Error messages are returned as JSON responses.
+
+## Deployment
+
+For deployment in a production environment, it is recommended to use a production-ready web server like Gunicorn or uWSGI behind a reverse proxy (e.g., Nginx). Ensure proper security measures are in place, such as securing API keys and setting up authentication.
+
+## License
+
+This project is licensed under the [MIT](https://choosealicense.com/licenses/mit/) License.
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import uuid
-from pydub import AudioSegment
 import tempfile
 from datetime import date
 from flask_cors import CORS
@@ -9,11 +8,7 @@ from sendgrid.helpers.mail import Mail
 from sendgrid import SendGridAPIClient
 import openai
 from dotenv import load_dotenv
-import certifi
-import ssl
-
-
-ssl_context = ssl.create_default_context(cafile=certifi.where())
+from pydub import AudioSegment
 
 app = Flask(__name__)
 CORS(app)
@@ -22,81 +17,73 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
 
 email_sender = SendGridAPIClient(os.getenv("SENDGRID_KEY"))
-
-email_template_path = 'transcript_email.html'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe_and_send_email():
     try:
-        email = request.form['email']
-        file = request.files['audio_file']
+        email = request.form.get('email')
+        file = request.files.get('audio_file')
 
-        if 'audio_file' not in request.files:
-            return jsonify({"error": "No file uploaded"}), 404
+        if not file or not email:
+            return jsonify({"error": "Missing required fields"}), 400
 
-        if file:
-            unique_filename = str(uuid.uuid4()) + '.' + file.filename.rsplit('.', 1)[1].lower()
-            upload_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
-            file.save(upload_path)
-            transcribe(upload_path, email)
-            return jsonify({"message": "Transcription request submitted successfully"}), 200
-        else:
-            return jsonify({"error": "Invalid file format"}), 401
+        if not allowed_file(file.filename):
+            return jsonify({"error": "Invalid File Format"}), 401
+
+        unique_filename = str(uuid.uuid4()) + '.' + file.filename.rsplit('.', 1)[1].lower()
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+        file.save(upload_path)
+        full_transcript = transcribe(upload_path)
+        summary = meeting_minutes(full_transcript)
+        send_email(email, summary, full_transcript)
+        return jsonify({"message": "Transcription request submitted successfully"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 def send_email(recipient, summary, transcript):
-    subject = f'Transcript - {str(date.today())}'
-    content = f"Summary: {summary['abstract_summary']}\n\nKey Points: {summary['key_points']}\n\nAction Items: {summary['action_items']}\n\nSentiment Analysis: {summary['sentiment']}\n\nTranscript:\n{transcript}"
-    message = Mail(
-        from_email='talktotextpro@gmail.com',
-        to_emails=recipient,
-        subject=subject,
-        plain_text_content=content
-    )
     try:
-        response = email_sender.send(message)
-        return response.status_code
+        subject = f'Transcript - {str(date.today())}'
+        message = Mail(
+            from_email='talktotextpro@gmail.com',
+            to_emails=recipient,
+            subject=subject,
+            html_content=f"Summary: {summary['abstract_summary']}\n\n{summary['key_points']}\n"
+                         f"\n{summary['action_items']}\n\nSentiment Analysis: {summary['sentiment']}\n"
+                         f"\nTranscript:\n{transcript}"
+        )
+        email_sender.send(message)
+        return jsonify({"message": "Transcription successful"}), 200
     except Exception as e:
-        print(f"Failed to send email to {recipient}. Error: {str(e)}")
+        return jsonify({"error": str(e)}), 500
 
 
 def allowed_file(filename):
-    allowed_extensions = {'mp3', 'wav', 'flac', 'ogg'}  # Add more extensions as needed
+    allowed_extensions = {'mp3', 'mp4', 'mpeg', 'wav', 'flac', 'ogg', 'mpga', 'm4a', 'webm'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
-def transcribe(file_path, email):
+def transcribe(file_path):
     try:
-        # Load the audio file
         audio = AudioSegment.from_file(file_path)
-
-        # Set the maximum segment size to 25 MB (25 * 1024 * 1024 bytes)
         max_segment_size = 20 * 500
 
-        # Create a temporary directory to store segments
         with tempfile.TemporaryDirectory() as temp_dir:
             segments = []
             transcriptions_segments = []
 
-            # Split the audio into segments
             start_time = 0
             while start_time < len(audio):
                 end_time = min(start_time + max_segment_size, len(audio))
                 segment = audio[start_time:end_time]
-
-                # Export the segment to a temporary file
                 temp_file_path = os.path.join(temp_dir, f"segment_{start_time}-{end_time}.wav")
                 segment.export(temp_file_path, format="wav")
-
                 segments.append(temp_file_path)
                 start_time = end_time
 
-            # Process and transcribe each segment
             for segment_path in segments:
                 with open(segment_path, 'rb') as segment_file:
                     transcription = openai.Audio.transcribe("whisper-1", segment_file)
@@ -104,14 +91,12 @@ def transcribe(file_path, email):
                 transcriptions_segments.append(transcription['text'])
                 os.remove(segment_path)
 
-            # Process the transcription as needed
-            summary = meeting_minutes(' '.join(transcriptions_segments))
-            send_email(email, summary, transcription['text'])
+            full_transcript = ' '.join(transcriptions_segments)
             os.remove(file_path)
+            return full_transcript
 
     except Exception as e:
-        print(str(e))
-        return None
+        return jsonify({"error": str(e)}), 500
 
 
 def meeting_minutes(transcription):
@@ -132,18 +117,13 @@ def abstract_summary_extraction(transcription):
         model="gpt-3.5-turbo",
         temperature=0,
         messages=[
-            {
-                "role": "system",
-                "content": "You are a highly skilled AI trained in language comprehension and summarization. I would "
-                           "like you to read the following text and summarize it into a concise abstract paragraph. "
-                           "Aim to retain the most important points, providing a coherent and readable summary that "
-                           "could help a person understand the main points of the discussion without needing to read "
-                           "the entire text. Please avoid unnecessary details or tangential points."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
+            {"role": "system", "content": "You are a highly skilled AI trained in language comprehension and "
+                                          "summarization. I would like you to read the following text and summarize "
+                                          "it into a concise abstract paragraph. Aim to retain the most important "
+                                          "points, providing a coherent and readable summary that could help a person "
+                                          "understand the main points of the discussion without needing to read the "
+                                          "entire text. Please avoid unnecessary details or tangential points."},
+            {"role": "user", "content": transcription}
         ]
     )
     return response['choices'][0]['message']['content']
@@ -154,18 +134,13 @@ def key_points_extraction(transcription):
         model="gpt-3.5-turbo",
         temperature=0,
         messages=[
-            {
-                "role": "system",
-                "content": "You are a proficient AI with a specialty in distilling information into key points. "
-                           "Based on the following text, identify and list the main points that were discussed or "
-                           "brought up. These should be the most important ideas, findings, or topics that are crucial "
-                           "to the essence of the discussion. Your goal is to provide a list that someone could read "
-                           "to quickly understand what was talked about."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
+            {"role": "system", "content": "You are a proficient AI with a specialty in distilling information into key"
+                                          " points. Based on the following text, identify and list the main points that"
+                                          " were discussed or brought up. These should be the most important ideas, "
+                                          "findings, or topics that are crucial to the essence of the discussion. Your "
+                                          "goal is to provide a list that someone could read to quickly understand what"
+                                          " was talked about."},
+            {"role": "user", "content": transcription}
         ]
     )
     return response['choices'][0]['message']['content']
@@ -176,18 +151,12 @@ def action_item_extraction(transcription):
         model="gpt-3.5-turbo",
         temperature=0,
         messages=[
-            {
-                "role": "system",
-                "content": "You are an AI expert in analyzing conversations and extracting action items. Please review "
-                           "the text and identify any tasks, assignments, or actions that were agreed upon or mentioned"
-                           " as needing to be done. These could be tasks assigned to specific individuals, or general"
-                           " actions that the group has decided to take. Please list these action items clearly and"
-                           " concisely."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
+            {"role": "system", "content": "You are an AI expert in analyzing conversations and extracting action items."
+                                          " Please review the text and identify any tasks, assignments, or actions that"
+                                          " were agreed upon or mentioned as needing to be done. These could be tasks"
+                                          " assigned to specific individuals, or general actions that the group has "
+                                          "decided to take. Please list these action items clearly and concisely."},
+            {"role": "user", "content": transcription}
         ]
     )
     return response['choices'][0]['message']['content']
@@ -198,22 +167,17 @@ def sentiment_analysis(transcription):
         model="gpt-3.5-turbo",
         temperature=0,
         messages=[
-            {
-                "role": "system",
-                "content": "As an AI with expertise in language and emotion analysis, your task is to analyze the "
-                           "sentiment of the following text. Please consider the overall tone of the discussion, "
-                           "the emotion conveyed by the language used, and the context in which words and phrases are "
-                           "used. Indicate whether the sentiment is generally positive, negative, or neutral, and "
-                           "provide brief explanations for your analysis where possible."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
+            {"role": "system", "content": "As an AI with expertise in language and emotion analysis, your task is to "
+                                          "analyze the sentiment of the following text. Please consider the overall "
+                                          "tone of the discussion, the emotion conveyed by the language used, and the "
+                                          "context in which words and phrases are used. Indicate whether the sentiment "
+                                          "is generally positive, negative, or neutral, and provide brief explanations"
+                                          " for your analysis where possible."},
+            {"role": "user", "content": transcription}
         ]
     )
     return response['choices'][0]['message']['content']
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
