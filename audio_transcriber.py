6810421a5a2a1073a639313b594abb73420346c7
Giridhar Nair <Giridhar.r.nair@gmail.com>
2023-10-11 22:21:23 -0500

    Chunked transcription

--- audio_transcriber.py
+++ audio_transcriber.py
@@ -96,68 +96,105 @@ def meeting_minutes(transcription):
     }
 
 
+def split_text_into_chunks(text, chunk_size=3000):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start = end
+    return chunks
+
+
 def abstract_summary_extraction(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {"role": "system", "content": "You are a highly skilled AI trained in language comprehension and "
-                                          "summarization. I would like you to read the following text and summarize "
-                                          "it into a concise abstract paragraph. Aim to retain the most important "
-                                          "points, providing a coherent and readable summary that could help a person "
-                                          "understand the main points of the discussion without needing to read the "
-                                          "entire text. Please avoid unnecessary details or tangential points."},
-            {"role": "user", "content": transcription}
-        ]
-    )
-    return response['choices'][0]['message']['content']
+    chunks = split_text_into_chunks(transcription)
+    summaries = []
+
+    for chunk in chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            messages=[
+                {"role": "system", "content": "You are a highly skilled AI trained in language comprehension and "
+                                              "summarization. I would like you to read the following text and summarize"
+                                              "it into a concise abstract paragraph. Aim to retain the most important "
+                                              "points, providing a coherent and readable summary that could help a "
+                                              "person understand the main points of the discussion without needing to "
+                                              "read the entire text. Please avoid unnecessary details or tangential "
+                                              "points."},
+                {"role": "user", "content": chunk}
+            ]
+        )
+        summaries.append(response['choices'][0]['message']['content'])
+
+    return ' '.join(summaries)
 
 
 def key_points_extraction(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {"role": "system", "content": "You are a proficient AI with a specialty in distilling information into key "
-                                          "points. Based on the following text, identify and list the main points that "
-                                          "were discussed or brought up. These should be the most important ideas, "
-                                          "findings, or topics that are crucial to the essence of the discussion. Your "
-                                          "goal is to provide a list that someone could read to quickly understand "
-                                          "what was talked about."},
-            {"role": "user", "content": transcription}
-        ]
-    )
-    return response['choices'][0]['message']['content']
+    chunks = split_text_into_chunks(transcription)
+    key_points = []
+
+    for chunk in chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            messages=[
+                {"role": "system", "content": "You are a proficient AI with a specialty in distilling information into "
+                                              "key points. Based on the following text, identify and list the main "
+                                              "points that were discussed or brought up. These should be the most "
+                                              "important ideas, findings, or topics that are crucial to the essence of "
+                                              "the discussion. Your goal is to provide a list that someone could read "
+                                              "to quickly understand what was talked about."},
+                {"role": "user", "content": chunk}
+            ]
+        )
+        key_points.append(response['choices'][0]['message']['content'])
+
+    return ' '.join(key_points)
 
 
 def action_item_extraction(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {"role": "system", "content": "You are an AI expert in analyzing conversations and extracting action items."
-                                          " Please review the text and identify any tasks, assignments, or actions that"
-                                          " were agreed upon or mentioned as needing to be done. These could be tasks"
-                                          " assigned to specific individuals, or general actions that the group has "
-                                          "decided to take. Please list these action items clearly and concisely."},
-            {"role": "user", "content": transcription}
-        ]
-    )
-    return response['choices'][0]['message']['content']
+    chunks = split_text_into_chunks(transcription)
+    action_items = []
+
+    for chunk in chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            messages=[
+                {"role": "system", "content": "You are an AI expert in analyzing conversations and extracting action "
+                                              "items. Please review the text and identify any tasks, assignments, or "
+                                              "actions that were agreed upon or mentioned as needing to be done. These "
+                                              "could be tasks assigned to specific individuals, or general actions "
+                                              "that the group has decided to take. Please list these action items "
+                                              "clearly and concisely."},
+                {"role": "user", "content": chunk}
+            ]
+        )
+        action_items.append(response['choices'][0]['message']['content'])
+
+    return ' '.join(action_items)
 
 
 def sentiment_analysis(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {"role": "system", "content": "As an AI with expertise in language and emotion analysis, your task is to "
-                                          "analyze the sentiment of the following text. Please consider the overall "
-                                          "tone of the discussion, the emotion conveyed by the language used, and the "
-                                          "context in which words and phrases are used. Indicate whether the sentiment "
-                                          "is generally positive, negative, or neutral, and provide brief explanations "
-                                          "for your analysis where possible."},
-            {"role": "user", "content": transcription}
-        ]
-    )
-    return response['choices'][0]['message']['content']
+    chunks = split_text_into_chunks(transcription)
+    sentiments = []
+
+    for chunk in chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            messages=[
+                {"role": "system", "content": "As an AI with expertise in language and emotion analysis, your task is "
+                                              "to analyze the sentiment of the following text. Please consider the "
+                                              "overall tone of the discussion, the emotion conveyed by the language "
+                                              "used, and the context in which words and phrases are used. Indicate "
+                                              "whether the sentiment is generally positive, negative, or neutral, and "
+                                              "provide brief explanations for your analysis where possible."},
+                {"role": "user", "content": chunk}
+            ]
+        )
+        sentiments.append(response['choices'][0]['message']['content'])
+
+    return ' '.join(sentiments)
